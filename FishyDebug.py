70d6ea0067b6922dfda36d317da391eefb2995ca
Tsumiman <tsumiman@gmail.com>
2018-01-28 04:25:20 -0600

    Debug module

---
+++ FishyDebug.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/ers/post_emergency', methods=['POST', 'GET'])
+def main():
+    print("Headers", request.headers)
+    print("User Agent", request.headers['User-Agent'])
+    print("Content length", request.content_length)
+    print("Content type", request.content_type)
+    print("Data", request.get_data())
+    print("JSON", request.get_json())
+    return Response(), 201
+
+if __name__ == '__main__':
+    app.run()
